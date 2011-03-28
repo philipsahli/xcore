b752e4ee56cd57563e0f294cacce445bb4cf617b
Philip Sahli <ilipcito@gmail.com>
2011-03-28 14:59:07 +0200

    add xpages

---
+++ mezzanine/xpages/migrations/0001_initial.py
@@ -0,0 +1,63 @@
+# encoding: utf-8
+from south.db import db
+from south.v2 import SchemaMigration
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'XPage'
+        db.create_table('xpages_xpage', (
+            ('page_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['pages.Page'], unique=True, primary_key=True)),
+            ('content', self.gf('mezzanine.core.fields.HtmlField')()),
+            ('splash_text', self.gf('mezzanine.core.fields.HtmlField')(null=True, blank=True)),
+            ('splash_image', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
+            ('with_splash', self.gf('django.db.models.fields.BooleanField')(default=True)),
+        ))
+        db.send_create_signal('xpages', ['XPage'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'XPage'
+        db.delete_table('xpages_xpage')
+
+
+    models = {
+        'core.keyword': {
+            'Meta': {'object_name': 'Keyword'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'pages.page': {
+            'Meta': {'ordering': "('titles',)", 'object_name': 'Page'},
+            '_keywords': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            '_order': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'content_model': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True'}),
+            'description': ('mezzanine.core.fields.HtmlField', [], {'blank': 'True'}),
+            'expiry_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'in_footer': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'in_navigation': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'keywords': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['core.Keyword']", 'symmetrical': 'False', 'blank': 'True'}),
+            'login_required': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['pages.Page']"}),
+            'publish_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'short_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'titles': ('django.db.models.fields.CharField', [], {'max_length': '1000', 'null': 'True'})
+        },
+        'xpages.xpage': {
+            'Meta': {'ordering': "('_order',)", 'object_name': 'XPage', '_ormbases': ['pages.Page']},
+            'content': ('mezzanine.core.fields.HtmlField', [], {}),
+            'page_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['pages.Page']", 'unique': 'True', 'primary_key': 'True'}),
+            'splash_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'splash_text': ('mezzanine.core.fields.HtmlField', [], {'null': 'True', 'blank': 'True'}),
+            'with_splash': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        }
+    }
+
+    complete_apps = ['xpages']
---
+++ mezzanine/xpages/models.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2011 Philip Sahli from sahli.net
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from mezzanine.pages.models import  Page
+from mezzanine.core.fields import HtmlField
+from mezzanine.core.models import Content
+from django.db.models import ImageField
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+class XPage(Page, Content):
+    splash_text = HtmlField(_("Splash"), null=True, blank=True)
+    splash_image = ImageField("Image", upload_to="splash/", blank=True, null=True)
+    with_splash = models.BooleanField(default=True)
+
+    class Meta:
+       verbose_name = _("XPage")
+       verbose_name_plural = _("XPages")
+
+#    @models.permalink
+#    def get_absolute_url(self):
+#        return ("page", (), {"slug": self.get_slug()})
+
+    def get_splash_image(self):
+        if not self.splash_image.name:
+            return ""
+        return "<img width='50px' class='img_block' src='%s'/>" % self.splash_image.url
