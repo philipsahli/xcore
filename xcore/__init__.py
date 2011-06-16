1818c3d4fac777144b768ee888b2a6637bcbb335
Philip Sahli <ilipcito@gmail.com>
2011-06-16 06:04:04 +0200

    add fabric command for dotcloud

--- xcore/__init__.py
+++ xcore/__init__.py
@@ -1,3 +1,8 @@
 # Activate urls
 __version__ = "0.11"
-import urls
+import os
+
+# fix for xcore, hook urls only when django env is set
+django_settings = os.environ.get('DJANGO_SETTINGS_MODULE')
+if django_settings is not None:
+    import urls
---
+++ xcore/utils/fabric/dotcloud.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2011 Philip Sahli from sahli.net
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from fabric.api import *
+
+def dotcloud_upgrade_pip():
+    local("dotcloud run %(dotcloud_service)s -- pip install --upgrade -r current/requirements.txt" % env)
+
+def dotcloud_push():
+    local("dotcloud push -b dotcloud %(dotcloud_service)s ." % env)
+
+def dotcloud_pip():
+    local("dotcloud run %(dotcloud_service)s -- pip install -r current/requirements.txt" % env)
+
+def dotcloud_deploy_upgrade():
+    dotcloud_push()
+    dotcloud_upgrade_pip()
+
+def dotcloud_deploy():
+    dotcloud_push()
+    dotcloud_pip()
+
+def dotcloud_syncdb():
+    local("dotcloud run %(dotcloud_service)s -- python current/manage.py syncdb" % env)
+
+
+def dotcloud_restart():
+    local("dotcloud restart %(dotcloud_service)s" % env)
