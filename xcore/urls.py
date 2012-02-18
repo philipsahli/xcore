4a7bd0d3c706c86ec76ec06192cc762ac5e272f5
Philip Sahli <ilipcito@gmail.com>
2012-02-18 17:36:55 +0100

    cleanup and fix urls

--- xcore/urls.py
+++ xcore/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 from django.utils.importlib import import_module
 import os
-from django.contrib.auth.views import login, logout, password_change, password_change_done, password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
 
 media_root = settings.MEDIA_ROOT
@@ -13,39 +12,16 @@ _urlconf = import_module(getattr(settings, "ROOT_URLCONF"))
 
 _urlpatterns = _urlconf.__getattribute__('urlpatterns')
 _tmp_urlpatterns = []
-_tmp_urlpatterns += patterns("",
-    url('^register/$', "xcore.profile.views.register", name="register"),
-    url('^login/$', login, {'template_name': 'xcore/login.html'}, name="login"),
-    url('^logout/$', logout, {'template_name': 'xcore/logout.html'}, name="logout"),
-    url('^loggedin/$', 'xcore.profile.views.loggedin'),
-    url('^accounts/profile/$', 'xcore.profile.views.profile', name="profile"),
-
-    #url('^set_lang/(?P<lang>.*)/$', 'xcore.utils.set_lang', name="set_lang"),
-
-    (r'^i18n/', include('django.conf.urls.i18n')),
-
-    (r'^resetpw/$', password_reset,
-         {'template_name': 'xcore/password_reset.html', 'email_template_name': 'xcore/password_reset_email.html',
-          'post_reset_redirect': '/resetpw/done/'}),
-    (r'^resetpw/done/$', password_reset_done, {'template_name': 'xcore/password_reset_done.html'}),
-    (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
-         {'template_name': 'xcore/password_reset_confirm.html'}),
-    (r'^reset/done/$', password_reset_complete, {'template_name': 'xcore/password_reset_complete.html'}),
-
-    url('^changepw/$', password_change, {'template_name': 'xcore/password_change.html'},
-        name="change_password"),
-    (r'^changepw_done/$', password_change_done, {'template_name': 'xcore/password_change_done.html'}),
+_urlpatterns += patterns("",
     (r"^label/(?P<key>.*).png", "xcore.label.views.get_label"),
-    #    (r"^maintenance$", "django.views.static.serve", {"path": "index.html", "document_root": maintenance_root, "show_indexes": False})
-    # (r'^', include("xcore.mezz.xpages.urls"))
-
 )
 
-try:
-    import mezzanine
-    urlpatterns = _tmp_urlpatterns
-except ImportError, e:
-    _urlpatterns += _tmp_urlpatterns
+#try:
+#    import mezzanine
+_urlconf.urlpatterns += _tmp_urlpatterns
+urlpatterns = _tmp_urlpatterns
+#except ImportError, e:
+#    _urlpatterns += _tmp_urlpatterns
 
 #for pattern in _urlpatterns:
 #    print pattern
