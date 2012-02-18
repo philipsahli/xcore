25a27529fd987aa3ac6ce234ed2f57aa5b87bfec
Philip Sahli <ilipcito@gmail.com>
2012-02-18 17:39:29 +0100

    remove app-media

--- xcore/app_media/js/xcore.js
+++
@@ -1,5 +0,0 @@
-/* clear the default value from a field */
-function clearText(field){
-    if (field.defaultValue == field.value) field.value = '';
-    else if (field.value == '') field.value = field.defaultValue;
-}
